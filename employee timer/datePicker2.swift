9f7a040650d3bd1d702fd8960f286b3212e35a2b
uri enat <homeUriEnat@h-imac-sl-uri.local>
2017-12-05 10:56:31 +0200

    xxx

--- employee timer/datePicker2.swift
+++ employee timer/datePicker2.swift
@@ -19,29 +19,21 @@ class datePicker2: UIViewController {
     var titleLbl = ""
     var recordToHandle = String()
     
-    let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+    //let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
     
     var locker = ""
-
     var employerID = ""
     var employeeID = ""
     var recordStatus = "save"
     
     var saveRecord : UIBarButtonItem?
-    //varibalwds for rounding
-    var roundSecond = 0
-    var roundMinute = 0
-    var roundHour = 0
-    var roundDay = 0
     
     let mydateFormat5 = DateFormatter()
     let mydateFormat10 = DateFormatter()
     let mydateFormat11 = DateFormatter()
 
     @IBAction func extendedDate1Button(_ sender: Any) {
-        
-        datePickerbBackground .isHidden = false
-
+    datePickerbBackground .isHidden = false
     }
     
     @IBOutlet weak var topOfStart: NSLayoutConstraint!
@@ -54,81 +46,62 @@ class datePicker2: UIViewController {
     @IBOutlet weak var DatePicker: UIDatePicker!
     @IBOutlet weak var Employer: UILabel!
     @IBOutlet weak var navBar: UINavigationBar!
-    
     @IBAction func pickerDone(_ sender: Any) {
         
     self.saveRecord = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.save, target: self, action: #selector(self.saveToDB2) )
     self.saveRecord?.isEnabled = true
     navigationItem.rightBarButtonItem = saveRecord
-
     datePickerChanged(DatePicker: DatePicker)
     }
     
-    
-    
-    var calcTimeIn = Date()
     var employerFromMain: String?
-    var outDate = String()
     var inDate = String()
     
     @IBAction func deleter(_ sender: Any) {
-        alert()
+    alert()
     }
-   
     @IBOutlet weak var deleter: UIBarButtonItem!
     
-    func bringRecord() {  dbRef.child(recordToHandle).observeSingleEvent(of:.value, with: { (snapshot) in
+    func bringRecord() {
+    dbRef.child(recordToHandle).observeSingleEvent(of:.value, with: { (snapshot) in
     if let dictionary = snapshot.value as? [String: String] {
     let record = recordsStruct()
     record.setValuesForKeys(dictionary)
-        print (dictionary)
+    print (dictionary)
+    self.startLbl.text = "Session";self.topOfStart.constant = 60;
         
-     self.startLbl.text = "Session";self.topOfStart.constant = 60;
-        
-        
-      
-        if ViewController.dateTimeFormat == "DateTime" { self.TimeIN.text = self.mydateFormat11.string(from: self.mydateFormat5.date(from: record.fIn!)!)} else {self.TimeIN.text = self.mydateFormat11.string(from: self.mydateFormat5.date(from: record.fIn!)!) }
-        self.inDate = record.fIn!
-        
-        
-        self.DatePicker.setDate( self.mydateFormat5.date(from: record.fIn!)!  , animated: true)
+    if ViewController.dateTimeFormat == "DateTime" { self.TimeIN.text = self.mydateFormat11.string(from: self.mydateFormat5.date(from: record.fIn!)!)} else {self.TimeIN.text = self.mydateFormat11.string(from: self.mydateFormat5.date(from: record.fIn!)!) }
+    self.inDate = record.fIn!
+    self.DatePicker.setDate( self.mydateFormat5.date(from: record.fIn!)!  , animated: true)
 
-
-       
-        self.locker = record.fStatus!
-        print (self.locker)
-        if self.locker == "Paid" {self.deleter.isEnabled = false;self.date1button.isEnabled = false; self.extendedDate1Button.isEnabled = false }
-        else {self.deleter.isEnabled = true;self.date1button.isEnabled = true; self.extendedDate1Button.isEnabled = true}
-        
-        self.titleLbl = "Edit record"
-        self.title = self.titleLbl
-        
-        self.employerID = record.fEmployerRef!
-        self.employeeID = record.fEmployeeRef!
-        }
-        }, withCancel: { (Error) in
-        print("error from FB123456")
-        })
-        
-        
+    self.locker = record.fStatus!
+    if self.locker == "Paid" {self.deleter.isEnabled = false;self.date1button.isEnabled = false; self.extendedDate1Button.isEnabled = false }
+    else {self.deleter.isEnabled = true;self.date1button.isEnabled = true; self.extendedDate1Button.isEnabled = true}
+    
+    self.titleLbl = "Edit Session"
+    self.title = self.titleLbl
+    
+    self.employerID = record.fEmployerRef!
+    self.employeeID = record.fEmployeeRef!
+    }
+    }, withCancel: { (Error) in
+    print("error from FB123456")
+    })
     }//end of func bring record
     
     /////////////////////////////////////////////////////////////////  view did load starts///////////////////////
     override func viewDidLoad() {
     super.viewDidLoad()
         
-        if ViewController.dateTimeFormat == "DateTime" {self.DatePicker.datePickerMode = .dateAndTime } else { self.DatePicker.datePickerMode = .date}
+    if ViewController.dateTimeFormat == "DateTime" {self.DatePicker.datePickerMode = .dateAndTime } else { self.DatePicker.datePickerMode = .date}
 
       
         //connectivity
         if Reachability.isConnectedToNetwork() == true
-        {
-            print("Internet Connection Available!")
-        }
-        else
-        {
-            print("Internet Connection not Available!")
-            alert50()
+        {print("Internet Connection Available!")
+        }else{
+        print("Internet Connection not Available!")
+        alert50()
         }
         
       
@@ -139,158 +112,91 @@ class datePicker2: UIViewController {
         
         if recordToHandle == "" {
         deleter.isEnabled = false
-            
         if ViewController.dateTimeFormat == "DateTime" { self.TimeIN.text = mydateFormat11.string(from: Date())} else {self.TimeIN.text = mydateFormat10.string(from: Date()) }
-            
+        
         titleLbl = "Add for " + employerFromMain!
-            
         startLbl.text = "Session";topOfStart.constant = 60;
          
-            self.saveRecord?.isEnabled = false;self.saveRecord = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.save, target: self, action: #selector(self.saveToDB2) )
+        self.saveRecord?.isEnabled = false;self.saveRecord = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.save, target: self, action: #selector(self.saveToDB2) )
         navigationItem.rightBarButtonItem = saveRecord
-
+        }else{
             
-        }//end of if
-        else{
-            deleter.isEnabled = true
-            
-            self.saveRecord = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.save, target: self, action: #selector(self.saveToDB2) )
-            
-            self.saveRecord?.isEnabled = false;
-            navigationItem.rightBarButtonItem = saveRecord
-
-
-
-            self.deleter.isEnabled = true
-           
-            bringRecord()
-
-            
-            
-
-            }//end of else
-        
-        
+        deleter.isEnabled = true
+        self.saveRecord = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.save, target: self, action: #selector(self.saveToDB2) )
+        self.saveRecord?.isEnabled = false;
         navigationItem.rightBarButtonItem = saveRecord
+        self.deleter.isEnabled = true
+        bringRecord()
+        }//end of else
         
-
-
-            self.title = titleLbl
+        self.title = titleLbl
  
     } ///end of did load/////////////////////////////////////////////////////////////////////////////////////////////////////
     
-    //override func viewDidAppear(_ animated: Bool) {
-        
-  //  }
-    
     func datePickerChanged(DatePicker:UIDatePicker) {
-        
-        datePickerbBackground .isHidden = true
-        if ViewController.dateTimeFormat == "DateTime" { self.TimeIN.text = mydateFormat11.string(from: DatePicker.date) } else {self.TimeIN.text = mydateFormat10.string(from: DatePicker.date) }
-        
-        }//end of func
-    
-   
-
+    datePickerbBackground .isHidden = true
+    if ViewController.dateTimeFormat == "DateTime" { self.TimeIN.text = mydateFormat11.string(from: DatePicker.date) } else {self.TimeIN.text = mydateFormat10.string(from: DatePicker.date) }
+    }//end of func
     
     func saveToDB2() {
-        navigationController!.popViewController(animated: true)
+    navigationController!.popViewController(animated: true)
 
-        
-            if recordToHandle == "" {
-                let record = ["fIn" : mydateFormat5.string(from: DatePicker.date), "fEmployer": String (describing : employerFromMain!),"fIndication3" :"✏️","fStatus" : "Pre","fEmployeeRef": String (describing : employeeID),"fEmployerRef":  String (describing : employerID)]
-                
-                let recordRefence = self.dbRef.childByAutoId()
-                recordRefence.setValue(record)
-                
-                
-                
-                
-                self.dbRefEmployee.child(self.employeeID).child("fEmployeeRecords").updateChildValues([recordRefence.key:Int(-(DatePicker.date.timeIntervalSince1970))])
-                self.dbRefEmployer.child(self.employerID).child("fEmployerRecords").updateChildValues([recordRefence.key:Int(-(DatePicker.date.timeIntervalSince1970))])            }//  end od if recors to handle is ""
-            else {
-                let record = ["fIn" : mydateFormat5.string(from: DatePicker.date),"fIndication3" :"✏️","fStatus" : "Pre" ]
-                dbRef.child(recordToHandle).updateChildValues(record)
-                self.dbRefEmployee.child(self.employeeID).child("fEmployeeRecords").updateChildValues([recordToHandle:Int(-(DatePicker.date.timeIntervalSince1970))])
-                self.dbRefEmployer.child(self.employerID).child("fEmployerRecords").updateChildValues([recordToHandle:Int(-(DatePicker.date.timeIntervalSince1970))])
-                
-            } //end od else
-            
-     
-            }
+    if recordToHandle == "" {
+    let record = ["fIn" : mydateFormat5.string(from: DatePicker.date), "fEmployer": String (describing : employerFromMain!),"fIndication3" :"✏️","fStatus" : "Pre","fEmployeeRef": String (describing : employeeID),"fEmployerRef":  String (describing : employerID)]
+    let recordRefence = self.dbRef.childByAutoId()
+    recordRefence.setValue(record)
     
-        @IBAction func cancelPressed(_ sender: AnyObject) {
-        }
-            
+    self.dbRefEmployee.child(self.employeeID).child("fEmployeeRecords").updateChildValues([recordRefence.key:Int(-(DatePicker.date.timeIntervalSince1970))])
+    self.dbRefEmployer.child(self.employerID).child("fEmployerRecords").updateChildValues([recordRefence.key:Int(-(DatePicker.date.timeIntervalSince1970))])
+    } else {
+    let record = ["fIn" : mydateFormat5.string(from: DatePicker.date),"fIndication3" :"✏️","fStatus" : "Pre" ]
+    dbRef.child(recordToHandle).updateChildValues(record)
+    self.dbRefEmployee.child(self.employeeID).child("fEmployeeRecords").updateChildValues([recordToHandle:Int(-(DatePicker.date.timeIntervalSince1970))])
+    self.dbRefEmployer.child(self.employerID).child("fEmployerRecords").updateChildValues([recordToHandle:Int(-(DatePicker.date.timeIntervalSince1970))])
+    } //end of else
+       
+    }//end of savetodb2
     
     @IBAction func date1(_ sender: AnyObject) {
-        datePickerbBackground .isHidden = false
-        }
+    datePickerbBackground .isHidden = false
+    }
     
-   
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        }
-    
-    // func for transforming int to h:m:"
-    func secondsTo (seconds : Int) -> (Int, Int) {
-        return (seconds / 3600, (seconds % 3600) / 60)
-        }
     
   ////alerts////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-    //deletealert
-     func alert () {
-        if recordStatus != "" {
-        print("delete")
-        let alertController = UIAlertController(title: "Delete", message: "This record would be deleted. Are You Sure?", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (UIAlertAction) in
-        //nothing
-        }
-            
-        let deleteAction = UIAlertAction(title: "Yes delete it.", style: .default) { (UIAlertAction) in
-        print ("delete it!!!")
-        self.dbRef.child(self.recordToHandle).removeValue()
-        self.dbRefEmployee.child(self.employeeID).child("fEmployeeRecords").child(self.recordToHandle).removeValue()
-        self.dbRefEmployer.child(self.employerID).child("fEmployerRecords").child(self.recordToHandle).removeValue()
-            
-            
-        self.navigationController!.popViewController(animated: true)
-            
-            
-        }
-     
-        alertController.addAction(cancelAction)
-        alertController.addAction(deleteAction)
-        self.present(alertController, animated: true, completion: nil)
-        }
-        
-        }//end of func alert
+//deletealert
+func alert () {
+if recordStatus != "" {
+print("delete")
+let alertController = UIAlertController(title: "Delete", message: "This record would be deleted. Are You Sure?", preferredStyle: .alert)
+let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (UIAlertAction) in
+//nothing
+}
+    
+let deleteAction = UIAlertAction(title: "Yes delete it.", style: .default) { (UIAlertAction) in
+print ("delete it!!!")
+self.dbRef.child(self.recordToHandle).removeValue()
+self.dbRefEmployee.child(self.employeeID).child("fEmployeeRecords").child(self.recordToHandle).removeValue()
+self.dbRefEmployer.child(self.employerID).child("fEmployerRecords").child(self.recordToHandle).removeValue()
 
+self.navigationController!.popViewController(animated: true)
+}
 
-    
-    
-    //Logic alert
-            func logicAlert () {
-            let alertController4 = UIAlertController(title: "Record's limit", message: "Sorry, but  24 hours is record's limit. Please update.", preferredStyle: .alert)
-            let OKAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
-                //nothing
-            }
-            alertController4.addAction(OKAction)
-        
-            self.present(alertController4, animated: true, completion: nil)
-            }//end of logic alert
-    
-    func alert50(){
-        let alertController50 = UIAlertController(title: ("Internet Connection") , message: " There is no internet - Check communication avilability.", preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
-        }
-        
-        alertController50.addAction(OKAction)
-        self.present(alertController50, animated: true, completion: nil)
-    }
-    
+alertController.addAction(cancelAction)
+alertController.addAction(deleteAction)
+self.present(alertController, animated: true, completion: nil)
+}
+}//end of func alert
 
+ 
+func alert50(){
+let alertController50 = UIAlertController(title: ("Internet Connection") , message: " There is no internet - Check communication avilability.", preferredStyle: .alert)
+let OKAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+}
+
+alertController50.addAction(OKAction)
+self.present(alertController50, animated: true, completion: nil)
+}
+    
 
 }///end!!!//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
